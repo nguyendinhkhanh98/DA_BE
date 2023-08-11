72325895a9547942b3912c9314af5a5ea62d04af
nguyen dinh khanh <khanhnd@ftech.ai>
2023-08-11 17:18:09 +0700

    update cloudinary

--- controller/api-v2/business-task.controller.js
+++ controller/api-v2/business-task.controller.js
@@ -136,8 +136,12 @@ module.exports.removeTaskHistory = async (req, res, next) => {
 
 module.exports.uploadAttackFile = async (req, res) => {
   // if (!fs.existsSync(dir)) shell.mkdir("-p", dir);
-  const result = await cloudinaryUtils.uploadSingle(req.file)
-  res.json(Formatter.success("upload_success", result));
+  try {
+    const result = await cloudinaryUtils.uploadSingle(req.file)
+    res.json(Formatter.success("upload_success", result));
+  } catch(err) {
+    res.json(Formatter.badRequest());
+  }
 };
 
 module.exports.deleteAttackFile = async (req, res) => {
